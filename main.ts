c299be34d3dfaa0fa7c403d8e27aee0f4c898d41
erichon <erichon18@gmail.com>
2021-04-01 00:00:42 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,19 @@
+pins.servoSetPulse(AnalogPin.P0, 3000)
+let forward = 1
+let backward = -1
+let direction = forward
+let speed = 1
+let max_angle = 135
+let min_angle = 45
+let angle = min_angle
+basic.forever(function () {
+    if (angle > max_angle) {
+        direction = backward
+    }
+    if (angle < min_angle) {
+        direction = forward
+    }
+    pins.servoWritePin(AnalogPin.P0, angle)
+    angle += direction
+    basic.pause(100)
+})
